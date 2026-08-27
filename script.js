869b0be46683dac27a67ffa8a4e6c80b73651304
emanuelydesouza-hue <emanuely.de.souza@escola.pr.gov.br>
2026-08-27 09:36:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-
 document.addEventListener("DOMContentLoaded", function () {
+    const dataPublicacao = document.getElementById("data-publicacao");
 
-    const elementoData = document.getElementById("data-publicacao");
+    const data = new Date(2026, 7, 20);
 
-    const dataPublicacao = new Date(2026, 7, 20);
-
-    const opcoes = {
+    dataPublicacao.textContent = data.toLocaleDateString("pt-BR", {
         day: "2-digit",
         month: "2-digit",
         year: "numeric"
-    };
-
-    elementoData.textContent =
-        dataPublicacao.toLocaleDateString("pt-BR", opcoes);
-
+    });
 });
